49cc9263bc872001dd17afbd535a3154ff3677be
Warakorn <junsongneanart@gmail.com>
2025-08-30 11:38:43 +0700

    Add HomeWork1 file2

--- HomeWork1/SelectSort.java
+++ HomeWork1/SelectSort.java
@@ -1,10 +1,10 @@
 package HomeWork1;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Date;
 
 public class SelectSort {
     public static void main(String[] args) {
-
         //Create scanner and Random object
         Scanner scan = new Scanner(System.in);
         Random rd = new Random();
@@ -28,16 +28,22 @@ public class SelectSort {
         selSort(arr);
 
         // Print the sorted array
-        System.out.print("Sorted array: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-           
+        //System.out.print("Sorted array: ");
+        //for (int i = 0; i < arr.length; i++) {
+            //System.out.print(arr[i] + " ");
+        //}
+        //System.out.println();
+        scan.close();   
     }
 
     //create method selSort
     public static void selSort(int a[]){
+
+        //time start
+        long start, stop;
+        Date time1 = new Date();
+        start = time1.getTime();
+
         // Selection sort algorith
         for(int i = 0; i < a.length-1; i++) {
             int min_idx = i;
@@ -51,5 +57,10 @@ public class SelectSort {
             a[min_idx] = a[i];
             a[i] = tmp;
         }
+        //time stop
+        Date time2 = new Date();
+        stop = time2.getTime();
+        double runTime = (stop - start)/1000.0;
+        System.out.println(runTime + " sec");
     }//end selSort method
 }
