e840ce97c0b72a520279acbe4379075e3e12b20d
Warakorn <junsongneanart@gmail.com>
2025-08-31 08:36:53 +0700

    Add date time

--- HomeWork1/SelectSort.java
+++ HomeWork1/SelectSort.java
@@ -6,6 +6,12 @@ import java.util.Date;
 public class SelectSort {
     public static void main(String[] args) {
         //Create scanner and Random object
+
+        //time start
+        long start, stop;
+        Date time1 = new Date();
+        start = time1.getTime();
+
         Scanner scan = new Scanner(System.in);
         Random rd = new Random();
 
@@ -14,36 +20,16 @@ public class SelectSort {
         int n = scan.nextInt();
 
         //create array size (n)
-        int arr[] = new int[n];
+        int a[] = new int[n];
 
         //loop random value in array
         System.out.print("Original array: ");
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = rd.nextInt(0,11);
-            System.out.print(arr[i] + " ");
+        for(int i = 0; i < a.length; i++) {
+            a[i] = rd.nextInt(0,11);
+            System.out.print(a[i] + " ");
         }
         System.out.println(); // Add a newline for better formatting
         
-        // Call the selSort method to sort the array in place
-        selSort(arr);
-
-        // Print the sorted array
-        System.out.print("Sorted array: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-        scan.close();   
-    }
-
-    //create method selSort
-    public static void selSort(int a[]){
-
-        //time start
-        long start, stop;
-        Date time1 = new Date();
-        start = time1.getTime();
-
         // Selection sort algorith
         for(int i = 0; i < a.length-1; i++) {
             int min_idx = i;
@@ -57,10 +43,20 @@ public class SelectSort {
             a[min_idx] = a[i];
             a[i] = tmp;
         }
-        //time stop
+
+        // Print the sorted array
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        scan.close();
+
+        //stop time
         Date time2 = new Date();
         stop = time2.getTime();
-        double runTime = (stop - start)/1000.0;
+        double runTime = (double)(stop - start)/1000.0;
+
         System.out.println(runTime + " sec");
-    }//end selSort method
+    }
 }
